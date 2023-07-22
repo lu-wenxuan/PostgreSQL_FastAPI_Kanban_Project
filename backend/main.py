2fd9071b3adec75cd66dfe8ed324cd2d3eb5d2e2
wenxuan lu <wenxuan_lu@outlook.com>
2023-07-22 23:32:23 +0800

    intial project and backend

---
+++ backend/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/board')
+def get_board() : 
+    board_data = { 
+        'task' : { 
+            "task-1" : {'id' : "task-1" , "content" : "created video"}, 
+            "task-2" : {"id" : "task-2" , "content" : "edit video"}, 
+        },
+        "columns" : { 
+            "column-1" : {
+                "id" : "column-1" , 
+                "title" : "todo" , 
+                "taskIds" : ["task-2" , "task1" ]
+            }, 
+            "column-2" : { 
+                "id" : "column-2", 
+                "title" :"Done Column" , 
+                "taskIds" : ["task-1"]
+            }
+        },
+        "columnOrder" : ["column-1" , "column-2"]
+    }
+    return {"board" : board_data}
